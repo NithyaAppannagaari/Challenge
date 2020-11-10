6952962d1ed60735cb4978f5c9f1d4352bb32c34
NithyaAppannagaari <70969819+NithyaAppannagaari@users.noreply.github.com>
2020-11-09 19:26:27 -0800

    Add files via upload

---
+++ Challenge.java
@@ -0,0 +1,16 @@
+public class Challenge
+{
+    public static void main(String[] args)
+    {
+        int num1, num2;
+        num1 = 1;
+        num2 = 3;
+        int product = num1*num2;
+        int sum = num1+num2;
+        int diff = num1-num2;
+        int div = num1/num2;
+        System.out.println(sum+" "+ diff + " "+ div+ " "+ product);
+        System.out.println("Nithya");
+    }
+
+}
